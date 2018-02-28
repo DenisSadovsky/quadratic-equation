180397cfa17a469f0e757bb3457ad316a7e0e51b
Denis - PC <denissadovsy93@gmail.com>
2018-03-01 00:08:46 +0300

    solution4

--- src/index.js
+++ src/index.js
@@ -1,3 +1,25 @@
 module.exports = function solveEquation(equation) {
-  // your implementation
+    result = [];
+
+    var value = equation.replace(/\s/g, "")
+                        .match(/[-]?\d+/g);
+    x1 = Number(value[0]);
+    x2 = Number(value[2]);
+    x3 = Number(value[3]);
+
+    sol = -4 * x1 * x3 + x2 * x2
+
+    var k = Math.sqrt(sol);
+
+    var1 = Math.round((-x2 + k) / 2 / x1);
+    var2 = Math.round((-x2 - k) / 2 / x1);
+
+    if(var1 < var2) {
+        result.push(var1);
+        result.push(var2);
+    } else {
+        result.push(var2);
+        result.push(var1);
+    }
+    return result;
 }
